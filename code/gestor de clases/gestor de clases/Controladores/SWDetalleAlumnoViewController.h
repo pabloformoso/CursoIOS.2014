fe6b97840565d77560feaa71ab90b59e4300bf8a
Pablo Formoso Estrada <pablo@pabloformoso.com>
2014-02-28 10:32:54 +0100

    28_02_v1

---
+++ code/gestor de clases/gestor de clases/Controladores/SWDetalleAlumnoViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWDetalleAlumnoViewController.h
+//  gestor de clases
+//
+//  Created by Pablo Formoso Estada on 28/02/14.
+//  Copyright (c) 2014 Pablo Formoso Estada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWDetalleAlumnoViewController : UIViewController
+
+@end
---
+++ code/gestor de clases/gestor de clases/Controladores/SWDetalleAlumnoViewController.m
@@ -0,0 +1,29 @@
+//
+//  SWDetalleAlumnoViewController.m
+//  gestor de clases
+//
+//  Created by Pablo Formoso Estada on 28/02/14.
+//  Copyright (c) 2014 Pablo Formoso Estada. All rights reserved.
+//
+
+#import "SWDetalleAlumnoViewController.h"
+
+@interface SWDetalleAlumnoViewController ()
+
+@end
+
+@implementation SWDetalleAlumnoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
